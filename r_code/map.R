9fa73ac7e1de126182a5fb6f955c782d43fced3a
Dan Lewer <danlewer@users.noreply.github.com>
2022-05-17 21:58:19 +0100

    Update map.R

--- r_code/map.R
+++ r_code/map.R
@@ -3,20 +3,22 @@ library(rgdal)
 library(maptools)
 gpclibPermit()
 library(ggplot2)
-library(viridisLite)
 
-tract <- readOGR("H:/irid_time_trends_HES_2022/lad_ug/Local_Authority_Districts_December_2017_Ultra_Generalised_Clipped_Boundaries_in_United_Kingdom_WGS84.shp")
+la_rate <- read.csv(url('https://raw.githubusercontent.com/danlewer/irid_trends/main/summary_tables/standardised_rate_by_LA_16may2022.csv'))
+la_rate <- la_rate[la_rate$period == 2017,]
 
-y <- fortify(tract, region = 'lad17cd')
+# from https://geoportal.statistics.gov.uk/datasets/ons::local-authority-districts-december-2017-boundaries-gb-buc/about
+tract <- readOGR('Local_Authority_Districts__December_2017__Boundaries_GB_BUC.shp')
+
+y <- fortify(tract, region = 'LAD17CD')
 y <- y[substr(y$id, 0, 1) == 'E',]
 
-cols <- magma(50)
-cols <- colorRampPalette(c('white', 'red'))(50)
+cols <- colorRampPalette(c('white', "#E41A1C", 'black'))(50)
 
 png('admission_rate_map.png', height = 8, width = 8, units = 'in', res = 300)
 
 ggplot() + 
-  geom_map(data = la_rate, aes(map_id=LAD10CD, fill = rate), map=y) +
+  geom_map(data = la_rate, aes(map_id=LAD10CD, fill = sr), map=y) +
   geom_path(aes(x = long, y = lat, group = group), data = y, color = 'black', lwd = 0.2) + 
   scale_fill_gradientn(colours = cols, na.value = 'blue') +
   theme_bw() + 
