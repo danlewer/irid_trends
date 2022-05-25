38a89b05c901c0de82f47b3f7dd53608f1d6054f
Dan Lewer <danlewer@users.noreply.github.com>
2022-05-25 14:55:13 +0100

    Update process_raw_HES.R

--- r_code/process_raw_HES.R
+++ r_code/process_raw_HES.R
@@ -43,7 +43,7 @@ icd10 <- list(
 # read patient-level HES data
 # ---------------------------
 
-d_raw <- fread("irid_hes_extract_17may2022.csv")
+d_raw <- fread("irid_hes_extract_24may2022.csv")
 
 d <- copy(d_raw)
 
@@ -66,7 +66,9 @@ d$ETHNOS[d$ETHNOS %in% c('R', 'S', 'Z', 'X', 7:9, 99)] <- 'Other or unknown'
 d[, ETHNOS := factor(ETHNOS, c('White', 'Black', 'Asian', 'Mixed', 'Other or unknown'))]
 
 # region
-d[, region := factor(RESGOR, c('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'U', 'Y', 'NULL', 'S', 'W', 'X', 'Z'), c('North East', 'North West', 'North West', 'Yorkshire & Humber', 'East Midlands', 'West Midlands', 'East of England', 'London', 'South East', 'South West', 'No fixed abode', 'Unknown', 'Unknown', 'Not England', 'Not England', 'Not England', 'Not England'))]
+d[, region1 := factor(RESGOR, c('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'U', 'Y', 'NULL', 'S', 'W', 'X', 'Z'), c('North East', 'North West', 'North West', 'Yorkshire & Humber', 'East Midlands', 'West Midlands', 'East of England', 'London', 'South East', 'South West', 'No fixed abode', 'Unknown', 'Unknown', 'Not England', 'Not England', 'Not England', 'Not England'))]
+d[, region2 := factor(RESGOR_ONS, c('E12000001', 'E12000002', 'E12000003', 'E12000004', 'E12000005', 'E12000006', 'E12000007', 'E12000008', 'E12000009', 'M99999999', 'NULL', 'U', 'W99999999', 'X', 'Y'), c('North East', 'North West', 'Yorkshire & Humber', 'East Midlands', 'West Midlands', 'East of England', 'London', 'South East', 'South West', 'Not England', 'Unknown', 'No fixed abode', 'Not England', 'Not England', 'Unknown'))]
+d[, region := fifelse(region1 == 'Unknown', as.character(region2), as.character(region1))]
 
 # imd
 d[, IMD04RK := as.numeric(d$IMD04RK)]
@@ -77,14 +79,6 @@ d[, imd5 := factor(imd5, c(1:5, 'missing'), c('1 - most deprived', 2:4, '5 - lea
 # dates
 date_cols <- c('ADMIDATE', 'DISDATE')
 d[, (date_cols) := lapply(.SD, format_date), .SDcols = date_cols]
-d[, LOS := as.integer(DISDATE - ADMIDATE) + 1] # this is more complete than SPELDUR
-d$LOS[d$LOS < 0] <- NA_integer_
-d[, SPELDUR := as.integer(SPELDUR)]
-d[, LOS := fifelse(is.na(LOS), SPELDUR + 1L, LOS)]
-LOS_lims <- c(1:5, 10)
-d[, LOS_group := findInterval(LOS, LOS_lims)]
-d$LOS_group[is.na(d$LOS_group)] <- 'missing'
-d[, LOS_group := factor(LOS_group, c(seq_along(LOS_lims), 'missing'), c(LOS_lims, 'missing'))]
 d[, year := year(ADMIDATE)]
 year_lims <- seq(2002, 2017, 5)
 d[, period := findInterval(year, year_lims)]
@@ -99,7 +93,7 @@ d[, admiMonth := paste0(month(ADMIDATE, label = T, abbr = T), ' ', year(ADMIDATE
 d[, admiMonth := factor(admiMonth, c(outer(month.abb, paste0(' ', 1997:2022), paste0)))]
 
 # primary cause
-diagpos <- sapply(sapply(icd10, paste0, collapse = '|'), grepl, d$DIAG4_01)
+diagpos <- sapply(sapply(icd10, paste0, collapse = '|'), grepl, d$DiagCode4)
 d[, diagnosis := names(icd10)[max.col(diagpos)]]
 d$diagnosis[rowSums(diagpos) == 0] <- 'missing'
 d[, diagnosis := factor(diagnosis, c(names(icd10), 'missing'))]
@@ -119,6 +113,17 @@ d <- disMeth[d, on = 'adID']
 # dummy variable
 d[, total := 1]
 
+# ===============
+# impute STARTAGE
+# ---------------
+
+d[, yearBirth := year - STARTAGE]
+d$yearBirth[d$yearBirth < 0] <- NA_integer_
+yearBirth <- d[, .(yearBirth = round(mean(yearBirth, na.rm = T), 0)), TOKEN_PERSON_ID]
+d[, yearBirth := NULL]
+d <- yearBirth[d, on = 'TOKEN_PERSON_ID']
+d[is.na(STARTAGE), table(is.na(yearBirth))] # doesn't really help
+
 # ===========================
 # exclude ineligible episodes
 # ---------------------------
@@ -132,17 +137,34 @@ d <- d[region != 'Not England']
 # exclusion criteria
 # ------------------
 
-nrow(d) # 98,240
+nrow(d) # 97576
 d[, .N, factor(findInterval(STARTAGE, c(0, 15, 65)), 1:3, c('0-14', '15-64', '65+'))]
-# 0-14 = 14; 65+ = 445
+# 0-14 = 10; 65+ = 443
 d <- d[STARTAGE >= 15 & STARTAGE <= 64]
-nrow(d) # 97,781
+nrow(d) # 97123
 d[, .N, epo]
-# planned = 3806; other = 1079
+# planned = 3772; other = 1048
 d <- d[epo == 'emergency']
-nrow(d) # 92896
+nrow(d) # 92303
 
 d <- droplevels(d)
+
+# =========================================
+# check reduction in March 2020 by pt group
+# -----------------------------------------
+
+abg <- Reduce(merge, lapply(c('total', 'age_group', 'region', 'diagnosis', 'SEX'), function (x) dcast(d, admiMonth ~ get(x), value.var = 'total', fun.aggregate = length)))
+
+par(mfrow = c(5, 6), mar = c(1, 0, 2, 0), oma = c(3, 3, 3, 3))
+lapply(names(abg)[-1], function (x) {
+  pd <- abg[stri_sub(admiMonth, 5, 8) > 2018]
+  plot(pd[, get(x)], type = 'b', ylim = c(0, max(pd[, get(x)])), axes = F, xlab = NA, ylab = NA, col = 'red')
+  # axis(1, seq_along(pd$admiMonth), pd$admiMonth, las = 2)
+  axis(1, seq_along(pd$admiMonth), labels = F)
+  axis(2, las = 2)
+  abline(v = which(pd[, admiMonth] == 'Mar 2020'))
+  title(main = x)
+}) # reduction around March 2020 appears across age groups, regions, diagnoses, and sex
 
 # ============================
 # table 1 and descriptive data
@@ -151,7 +173,7 @@ d <- droplevels(d)
 # age histogram
 
 age_hist <- d[, .(n = pmax(10, .N)), STARTAGE][order(STARTAGE)]
-fwrite(age_hist, 'age_histogram_16may2022.csv')
+fwrite(age_hist, 'age_histogram_25may2022.csv')
 
 cv <- function (v) {
   a <- d[, table(get(v))]
@@ -182,13 +204,11 @@ table1 <- rbind(cv('total'),
                 cv('ETHNOS'), 
                 cv('imd5'), 
                 cv('region'),
-                cv('LOS_group'),
-                nv('LOS'),
                 cv('finalDis'),
                 cv('diagnosis'),
                 cv('period'))
 
-fwrite(table1, 'table1_16may2022.csv')
+fwrite(table1, 'table1_25may2022.csv')
 
 # =============
 # age quantiles
@@ -199,7 +219,7 @@ age_quantiles <- rbind(aggregate(d$STARTAGE, by = list(region = d$region, year =
 age_quantiles <- data.table(age_quantiles)
 age_quantiles <- age_quantiles[year %in% 1998:2021]
 names(age_quantiles) <- c('region', 'year', 'q0.05', 'q0.25', 'q0.5', 'q0.75', 'q0.95')
-fwrite(age_quantiles, 'age_quantiles_16may2022.csv')
+fwrite(age_quantiles, 'age_quantiles_25may2022.csv')
 
 # ===========
 # time trends
@@ -208,7 +228,7 @@ fwrite(age_quantiles, 'age_quantiles_16may2022.csv')
 yrAge <- d[year >= 1998 & year <= 2021, .(admissions = .N), c('year', 'age_group')]
 # redact small cells (very few of these)
 yrAge[, admissions := pmax(10, admissions)]
-fwrite(yrAge, 'admissions_by_year_and_age_group_16may2022.csv')
+fwrite(yrAge, 'admissions_by_year_and_age_group_25may2022.csv')
 
 # ======================
 # local authority counts
@@ -288,7 +308,7 @@ simSR <- rbind(la_count[, .(sr = sum(e) / sum(refpop) * 1000000), c('period', 'L
                la_count[, .(sr = sum(e) / sum(refpop) * 1000000), 'LAD10CD'], fill = T)[simSR, on = c('period', 'LAD10CD')]
 simSR <- la_lookup[simSR, on = 'LAD10CD']
 
-fwrite(simSR, 'standardised_rate_by_LA_18may2022.csv')
+fwrite(simSR, 'standardised_rate_by_LA_25may2022.csv')
 
 # =================
 # estimate of costs
@@ -304,66 +324,60 @@ yr_diag[, cost2 := scaledN * cost]
 yr_diag[region == 'London', sum(scaledN), year][order(year)] # slightly lower than 2011 estimate based on rate of hospitalisation in SLAM cohort
 yr_diag[year %in% 1998:2021, .(total_cost = sum(cost2)), year][order(year)]
 
-#    year total_cost
-# 1: 2002   32064274
-# 2: 2003   42601445
-# 3: 2004   39939118
-# 4: 2005   35736869
-# 5: 2006   35717421
-# 6: 2007   34279994
-# 7: 2008   35145964
-# 8: 2009   30926505
-# 9: 2010   31828843
-# 10: 2011   28169732
-# 11: 2012   30037153
-# 12: 2013   34341237
-# 13: 2014   40967363
-# 14: 2015   47907460
-# 15: 2016   55946814
-# 16: 2017   64080143
-# 17: 2018   69542125
-# 18: 2019   69610960
-# 19: 2020   51427388
-# 20: 2021   45805940
+# year total_cost
+# 1: 2002   31731412
+# 2: 2003   42241397
+# 3: 2004   39658946
+# 4: 2005   35445807
+# 5: 2006   35308171
+# 6: 2007   33912071
+# 7: 2008   34842453
+# 8: 2009   30799484
+# 9: 2010   31448206
+# 10: 2011   28074978
+# 11: 2012   29938317
+# 12: 2013   34260933
+# 13: 2014   40928177
+# 14: 2015   47651939
+# 15: 2016   55633435
+# 16: 2017   63769101
+# 17: 2018   69218413
+# 18: 2019   69123223
+# 19: 2020   51162972
+# 20: 2021   45760018
 
 #  ========
 #  COVID-19
 #  --------
 
 covid19 <- d[year > 2017 & year < 2022, .N, admiMonth]
-fwrite(covid19, 'monthly_from_jan2018_16may2022.csv')
+fwrite(covid19, 'monthly_from_jan2018_25may2022.csv')
 
 #  =============================================
 #  national trends for other types of admissions
 #  ---------------------------------------------
 
-fl <- list(
-  all_cause = "emergency_admissions_per_day.csv",
-  substances = "emergency_admissions_psychoactive_substances.csv", # F10-F19 - Psychoactive substance use
-  alcohol = "emergency_admissions_alcohol.csv", # F10 - Alcohol
-  appendix = "emergency_admissions_appendix.csv", # K35-K38 - Diseases of the appendix including acute apendicitis
-  asthma = "emergency_admissions_asthma.csv", # J45 # Asthma
-  diabetes = "emergency_admissions_diabetes.csv", # E10-E14 # Diabetes mellitus
-  dorsopathies = "emergency_admissions_dorsopathies.csv", # M40-M54 # Dorsopathies
-  episodic_paroxysmal = "emergency_admissions_episodic_paroxysmal.csv", # G40-G47 # Episodic and paroxysmal disorders, including epilepsy
-  enteritis = "emergency_admissions_noninfective_enteritis.csv", #K50-K52 # Noninfective enteritis and colitis
-  urinary = "emergency_admissions_urinary.csv" #N30-N39
+nt <- fread('emergency_admissions_per_month_25may2022.csv')
+nt[, dt := ymd(paste(AdmiMonth, '-01'))]
+nt <- nt[year(dt) >= 2018]
+nt[, AdmiMonth := paste0( month(dt, label = T), ' ', year(dt))]
+nt[, AdmiMonth := factor(AdmiMonth, c(outer(month.abb, paste0(' ', 2018:2022), paste0)))]
+
+abm <- list(
+  nt[, .(all_cause = sum(admissions)), AdmiMonth],
+  nt[DIAG3_01 == 'F10', .(alcohol = sum(admissions)), AdmiMonth],
+  nt[DIAG3_01 %in% setdiff(paste0('F', 10:19), c('F10', 'F17')), .(drugs = sum(admissions)), AdmiMonth],
+  nt[DIAG3_01 == 'J45', .(asthma = sum(admissions)), AdmiMonth],
+  nt[DIAG3_01 %in% c('K35', 'K36', 'K37', 'K38'), .(appendix = sum(admissions)), AdmiMonth],
+  nt[stri_sub(DIAG3_01, 0, 1) %in% c('A', 'B'), .(infections = sum(admissions)), AdmiMonth],
+  nt[DIAG3_01 %in% paste0('J', stri_pad(0:22, width = 2, pad = '0')), .(resp_infections = sum(admissions)), AdmiMonth],
+  nt[DIAG3_01 %in% paste0('A', stri_pad(0:9, width = 2, pad = '0')), .(intestinal_infections = sum(admissions)), AdmiMonth],
+  nt[DIAG3_01 %in% paste0('A', 15:19), .(tb = sum(admissions)), AdmiMonth],
+  nt[DIAG3_01 %in% c('E10', 'E11', 'E12', 'E13', 'E14'), .(diabetes = sum(admissions)), AdmiMonth],
+  nt[DIAG3_01 == 'U07', .(covid = sum(admissions)), AdmiMonth]
 )
+abm <- Reduce(function(...) merge(..., all = TRUE), abm)
+abm <- abm[order(AdmiMonth)]
+abm <- cbind(AdmiMonth = abm$AdmiMonth, abm[, lapply(.SD, pmax.int, y = 10)][, -1])
 
-ad <- lapply(fl, fread)
-for(i in seq_along(ad)) {
-  ad[[i]]$ADMIDATE <- format_date(ad[[i]]$ADMIDATE)
-  ad[[i]]$admiMonth <- paste0(month(ad[[i]]$ADMIDATE, label = T, abbr = T), ' ', year(ad[[i]]$ADMIDATE))
-  ad[[i]] <- ad[[i]][, sum(admissions), admiMonth]
-  names(ad[[i]])[2] <- names(fl)[i]
-}
-ad <- Reduce(merge, ad)
-ad[, admiMonth := factor(admiMonth, levels(d$admiMonth))]
-ad <- ad[order(admiMonth)]
-ad <- ad[!is.na(admiMonth)]
-ad <- ad[stri_sub(admiMonth, 5, 9) %in% as.character(1998:2021)]
-
-# check no small counts
-sapply(ad[, names(fl), with = F], range)
-
-fwrite(ad, 'monthly_trends_other_causes_16may2022.csv')
+fwrite(abm, 'monthly_trends_other_causes_25may2022.csv')
