317af6a01ba751f28f16c7fa8985586e9253b009
Dan Lewer <danlewer@users.noreply.github.com>
2022-10-27 21:55:14 +0100

    Update HES_query_comparison_counts.sql

--- HES_query_comparison_counts.sql
+++ HES_query_comparison_counts.sql
@@ -1,4 +1,4 @@
-/* staph: all */
+/* staph and strep sin infections; not opioid related */
 
 SELECT ADMIDATE,
 COUNT (*) admissions
@@ -6,67 +6,14 @@ FROM HES_APC.dbo.[vtHES_APC]
 WHERE EPIORDER = 1
 AND EPISTAT = 3
 AND ADMIMETH IN ('21', '22', '23', '24', '25', '2A', '2B', '2C', '2D')
-AND EPIKEY IN (SELECT diag.EPIKEY
-		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag
-		WHERE diag.DiagCode4 IN ('B956', 'B957', 'B958'))
-GROUP BY ADMIDATE
-
-/* staph: skin */
-
-SELECT ADMIDATE,
-COUNT (*) admissions
-FROM HES_APC.dbo.[vtHES_APC]
-WHERE EPIORDER = 1
-AND EPISTAT = 3
-AND ADMIMETH IN ('21', '22', '23', '24', '25', '2A', '2B', '2C', '2D')
-AND EPIKEY IN (SELECT diag.EPIKEY
-		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag
-		WHERE diag.DiagCode4 IN ('B956', 'B957', 'B958'))
-AND EPIKEY IN (SELECT diag.EPIKEY
-		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag
-		WHERE diag.DiagCode4 LIKE 'L%'
-		AND diag.Diagidx = 1)
-GROUP BY ADMIDATE
-
-/* strep: all */
-
-SELECT ADMIDATE,
-COUNT (*) admissions
-FROM HES_APC.dbo.[vtHES_APC]
-WHERE EPIORDER = 1
-AND EPISTAT = 3
-AND ADMIMETH IN ('21', '22', '23', '24', '25', '2A', '2B', '2C', '2D')
-AND EPIKEY IN (SELECT diag.EPIKEY
-		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag
-		WHERE diag.DiagCode4 IN ('B950', 'B951', 'B952', 'B953', 'B954', 'B955'))
-GROUP BY ADMIDATE
-
-/* strep: skin */
-
-SELECT ADMIDATE,
-COUNT (*) admissions
-FROM HES_APC.dbo.[vtHES_APC]
-WHERE EPIORDER = 1
-AND EPISTAT = 3
-AND ADMIMETH IN ('21', '22', '23', '24', '25', '2A', '2B', '2C', '2D')
-AND EPIKEY IN (SELECT diag.EPIKEY
-		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag
-		WHERE diag.DiagCode4 IN ('B950', 'B951', 'B952', 'B953', 'B954', 'B955'))
-AND EPIKEY IN (SELECT diag.EPIKEY
-		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag
-		WHERE diag.DiagCode4 LIKE 'L%'
-		AND diag.Diagidx = 1)
-GROUP BY ADMIDATE
-
-/* F11 */
-
-SELECT ADMIDATE,
-COUNT (*) admissions
-FROM HES_APC.dbo.[vHES_APC_Flat]
-WHERE DIAG3_01 = 'F11'
-AND EPIORDER = 1
-AND EPISTAT = 3
-AND ADMIMETH IN ('21', '22', '23', '24', '25', '2A', '2B', '2C', '2D')
+AND EPIKEY IN (SELECT diag1.EPIKEY
+		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag1
+		WHERE diag1.DiagCode4 LIKE 'B95%'
+		AND diag1.DiagCode4 NOT LIKE 'F11%')
+AND EPIKEY IN (SELECT diag2.EPIKEY
+		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag2
+		WHERE diag2.DiagCode4 LIKE 'L%'
+		AND diag2.Diagidx = 1)
 GROUP BY ADMIDATE
 
 /* T-codes */
@@ -81,4 +28,21 @@ AND EPIKEY IN (SELECT diag.EPIKEY
 		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag
 		WHERE diag.DiagCode4 IN ('T400', 'T401', 'T402', 'T403', 'T404')
 		AND diag.DiagIdx = 1)
+GROUP BY ADMIDATE
+
+/* Other overdoses */
+
+SELECT ADMIDATE,
+COUNT (*) admissions
+FROM HES_APC.dbo.[vtHES_APC]
+WHERE EPIORDER = 1
+AND EPISTAT = 3
+AND ADMIMETH IN ('21', '22', '23', '24', '25', '2A', '2B', '2C', '2D')
+AND EPIKEY IN (SELECT diag.EPIKEY
+		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag
+		WHERE diag.DiagCode4 IN ('T400', 'T401', 'T402', 'T403', 'T404'))
+AND EPIKEY IN (SELECT diag1.EPIKEY
+		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag1
+		WHERE diag1.DiagCode4 LIKE 'F11%'
+		AND diag1.DiagIdx = 1)
 GROUP BY ADMIDATE
