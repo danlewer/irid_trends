9b9a7270616d28ad0849db7bf747af05d89a086b
Dan Lewer <danlewer@users.noreply.github.com>
2023-02-18 16:45:22 +0000

    Update comparison_counts.sql

--- sql_queries/comparison_counts.sql
+++ sql_queries/comparison_counts.sql
@@ -1,3 +1,21 @@
+/* staph and strep sin infections; not opioid related */
+
+SELECT ADMIDATE,
+COUNT (*) admissions
+FROM HES_APC.dbo.[vtHES_APC]
+WHERE EPIORDER = 1
+AND EPISTAT = 3
+AND ADMIMETH IN ('21', '22', '23', '24', '25', '2A', '2B', '2C', '2D')
+AND EPIKEY IN (SELECT diag1.EPIKEY
+		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag1
+		WHERE diag1.DiagCode4 LIKE 'B95%'
+		AND diag1.DiagCode4 NOT LIKE 'F11%')
+AND EPIKEY IN (SELECT diag2.EPIKEY
+		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag2
+		WHERE diag2.DiagCode4 LIKE 'L%'
+		AND diag2.Diagidx = 1)
+GROUP BY ADMIDATE
+
 /* measure 1 - hospital admissions with a primary diagnosis of drug-related mental and behavioural disorders */
 
 SELECT ADMIDATE,
