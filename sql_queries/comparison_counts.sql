65e9d975a8d85c0fab27909d07fe75d3f8d14575
Dan Lewer <danlewer@users.noreply.github.com>
2023-02-21 22:16:18 +0000

    Update comparison_counts.sql

--- sql_queries/comparison_counts.sql
+++ sql_queries/comparison_counts.sql
@@ -1,4 +1,4 @@
-/* staph and strep sin infections; not opioid related */
+-- staph and strep sin infections; not opioid related
 
 SELECT ADMIDATE,
 COUNT (*) admissions
@@ -16,7 +16,7 @@ AND EPIKEY IN (SELECT diag2.EPIKEY
 		AND diag2.Diagidx = 1)
 GROUP BY ADMIDATE
 
-/* measure 1 - hospital admissions with a primary diagnosis of drug-related mental and behavioural disorders */
+-- measure 1 - hospital admissions with a primary diagnosis of drug-related mental and behavioural disorders
 
 SELECT ADMIDATE,
 COUNT (*) admissions
@@ -32,7 +32,7 @@ AND EPIKEY IN (SELECT diag.EPIKEY
 		AND diag.DiagIdx = 1)
 GROUP BY ADMIDATE
 
-/* measure 2 - hospital admissions with a primary diagnosis of poisoning by drugs */
+-- measure 2 - hospital admissions with a primary diagnosis of poisoning by drugs
 
 SELECT ADMIDATE,
 COUNT (*) admissions
@@ -44,5 +44,19 @@ AND EPIKEY IN (SELECT diag.EPIKEY
 		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag
 		WHERE diag.DiagCode4 LIKE 'T40%'
 		OR diag.DiagCode4 = 'T436'
+		AND diag.DiagIdx = 1)
+GROUP BY ADMIDATE
+
+-- F11 only
+
+SELECT ADMIDATE,
+COUNT (*) admissions
+FROM HES_APC.dbo.[vtHES_APC]
+WHERE EPIORDER = 1
+AND EPISTAT = 3
+AND ADMIMETH IN ('21', '22', '23', '24', '25', '2A', '2B', '2C', '2D')
+AND EPIKEY IN (SELECT diag.EPIKEY
+		FROM HES_APC.dbo.[vtHES_APC_DIAG] AS diag
+		WHERE diag.DiagCode4 LIKE 'F11%'
 		AND diag.DiagIdx = 1)
 GROUP BY ADMIDATE
